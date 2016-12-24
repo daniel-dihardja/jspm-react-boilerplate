30e378284bf038ea027bc2f7e8afd60b16d04069
Daniel Dihardja <daniel@dihardja.de>
2016-12-24 12:16:45 +0100

    testing the navigator

--- src/components/app.js
+++ src/components/app.js
@@ -8,35 +8,62 @@ import Home from './states/home';
 import About from './states/about';
 import Contact from './states/contact';
 
-import {Page, Button, Toolbar, ToolbarButton, Icon, BackButton} from 'react-onsenui';
-
 import '../jspm_packages/npm/onsenui@2.0.5/css/onsenui.css!';
 import '../jspm_packages/npm/onsenui@2.0.5/css/onsen-css-components.css!';
 
 import ons from 'onsenui';
+import {Navigator, Toolbar, Page, Button, BackButton} from 'react-onsenui';
 
 class App extends React.Component {
 
-    onClick() {
-        ons.notification.alert('Hello world!');
+    constructor(props) {
+        super(props);
+        this.index = 0;
+    }
+
+    goBack(navigator) {
+        navigator.popPage();
+    }
+
+    renderToolbar(route, navigator) {
+        const backButton = route.hasBackButton ? <BackButton onClick={this.goBack.bind(this, navigator)}>Back</BackButton> : null;
+        return (
+            <Toolbar>
+                <div className="left">{backButton}</div>
+                <div className="center">{route.title}</div>
+            </Toolbar>
+        );
+    }
+
+    pushPage(navigator) {
+        navigator.pushPage({
+            title: `Another Page ${this.index}`,
+            hasBackButton: true
+        });
+        this.index ++;
+    }
+
+    renderPage(route, navigator) {
+        return(
+            <Page key={route.title} renderToolbar={this.renderToolbar.bind(this, route, navigator)}>
+                <section style={{margin: '16px', textAlign: 'center'}}>
+                    <Button onClick={this.pushPage.bind(this, navigator)}>
+                        Push Page
+                    </Button>
+                </section>
+            </Page>
+        );
     }
 
     render() {
         return (
-            <Page
-                renderToolbar={ () =>
-                    <Toolbar>
-                        <div className='left'>
-                            <BackButton>Back</BackButton>
-                        </div>
-                        <div className='center'>Home</div>
-                        <div className='right'>
-                            <ToolbarButton onClick={this.onClick.bind(this)}>
-                                <Icon icon='md-more-vert' />
-                            </ToolbarButton></div>
-                    </Toolbar>
-                }>
-            </Page>
+            <Navigator
+                renderPage={this.renderPage.bind(this)}
+                initialRoute={{
+                    title: 'First Page',
+                    hasBackButton: false
+                }}>
+            </Navigator>
         );
     }
 }
