201daa865a9a25401b547849a9745c69c5f5e36d
Daniel Dihardja <daniel@dihardja.de>
2017-01-05 13:01:03 +0100

    added mocha for unit tests

---
+++ test/unit/demo.test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by danieldihardja on 03/01/17.
+ */
+
+// TODO:
+// find a workaround to load es6 modules
+// import contentDb from '../../src/components/services/content-db';
+
+describe('demo', function() {
+    describe('test1', function() {
+        it('should pass', function(done) {
+            done();
+        })
+    });
+});
